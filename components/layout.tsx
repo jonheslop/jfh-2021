546cb90dd3865bd45a2b68460ab52af8156c63f3
jonheslop <jon.heslop@pusher.com>
2021-03-03 17:40:48 +0000

    Fix wrong syntax

--- components/layout.tsx
+++ components/layout.tsx
@@ -14,12 +14,12 @@ export default function Layout({pageTitle, children}: Props) {
         <title>{pageTitle} - Jon Heslop</title>
         <link rel="icon" href="/favicon.gif" />
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-        <meta name="description">Jon Heslop is a designer & developer based in London.</meta>
+        <meta name="description" content="Jon Heslop is a designer & developer based in London." />
         <meta property="og:url" content="http://jonheslop.com/" />
         <meta property="og:title" content="Jon Heslop" />
         <meta property="og:description" content="Jon Heslop is a designer & developer based in London." />
         <meta property="og:type" content="website" />
-        <meta property="og:image" content="/jon-heslop.jpg"/>
+        <meta property="og:image" content="/jon-heslop.jpg" />
       </Head>
 
       <div className="flex-1 p-16 grid grid-cols-1 md:grid-cols-page-layout auto-rows-min items-start gap-16 relative">
