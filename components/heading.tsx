fc0cf9946792d0443fe76c15ebf7f86e63a06e34
jonheslop <jon.heslop@pusher.com>
2021-03-04 12:14:00 +0000

    Prefer double quotes

--- components/heading.tsx
+++ components/heading.tsx
@@ -6,20 +6,21 @@ type Props = {
   children: React.ReactNode
 }
 
-export default function Heading({level = 'h2', classes = '', children}: Props) {
+export default function Heading({level = "h2", classes = "", children}: Props) {
   const Tag = level;
   let baseClasses:string;
   switch (level) {
-    case 'h1':
+    case "h1":
       baseClasses = "text-4xl md:text-3xl lg:text-4xl xl:text-5xl"
       break;
-    case 'h4':
-      baseClasses = "md:text-xl md:text-lg lg:text-xl text-gray-500"
+    case "h4":
+      baseClasses = "text-xl md:text-lg lg:text-xl text-gray-500"
       break;
     default:
       baseClasses = "text-4xl md:text-2xl lg:text-4xl"
       break;
   }
+
   return (
     <Tag className={`${baseClasses} ${classes}`}>
       {children}
