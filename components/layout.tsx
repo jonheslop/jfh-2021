6aa424d6abcf9ea48ff0b86d02cadc6fb6c5ea09
jonheslop <jon.heslop@pusher.com>
2021-03-04 09:16:32 +0000

    Preconnect to CDN for fonts

--- components/layout.tsx
+++ components/layout.tsx
@@ -12,6 +12,7 @@ export default function Layout({pageTitle, children}: Props) {
     <div className="flex flex-col items-center justify-center min-h-screen font-soehne">
       <Head>
         <title>{pageTitle} - Jon Heslop</title>
+        <link rel="preconnect" href="https://cdn.jonheslop.com"></link>
         <link rel="icon" href="/favicon.gif" />
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
         <meta name="description" content="Jon Heslop is a designer & developer based in London." />
