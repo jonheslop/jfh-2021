ec51559c8f2471b58b0f7d52a029bd38ed4356f0
Jon Heslop <jon@jonheslop.com>
2021-12-17 15:27:25 +0000

    Same for footer

--- components/footer.tsx
+++ components/footer.tsx
@@ -3,7 +3,7 @@ import Link from './link';
 
 const Footer = () => {
   return (
-    <footer className="flex flex-wrap items-center w-full p-8 md:p-16 text-xs md:text-sm justify-between">
+    <footer className="flex flex-wrap items-center w-full px-2 py-8 md:p-8 lg:p-16 text-xs md:text-sm justify-between">
       <section className="flex flex-wrap space-x-6 space-x-reverse mt-4 sm:mt-0 pb-2 border-b sm:pb-0 sm:border-none">
         <Link external classes="my-2 lg:my-0 mr-6" href="https://github.com/jonheslop">Github</Link>
         <Link external classes="my-2 lg:my-0" href="https://instagram.com/jonheslop">Instagram</Link>
