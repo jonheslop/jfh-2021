b07cbd7f52a6cf8b97c0cd867d8f2440b3d952c9
Jon Heslop <jon@jonheslop.com>
2023-09-14 15:24:23 +0100

    Add image ids so can link to them

--- pages/posts/[slug].js
+++ pages/posts/[slug].js
@@ -50,12 +50,13 @@ export default function Post({ post, morePosts }) {
         </>
       )}
       {post.photos !== undefined && (
-        <div className="md:col-start-2 col-span-2 md:col-span-3 w-full relative grid gap-8 md:gap-16">
-          {post.photos.map((photo) => (
+        <div className="md:col-start-2 col-span-2 md:col-span-3 w-full relative grid gap-4 md:gap-8">
+          {post.photos.map((photo, i) => (
             // eslint-disable-next-line @next/next/no-img-element
             <img
-              className="mx-auto"
+              className="mx-auto py-2 md:py-4"
               alt=""
+              id={`image-${i}`}
               key={photo}
               src={`https://imagedelivery.net/tfgleCjJafHVtd2F4ngDnQ/${photo}/small`}
               srcSet={`https://imagedelivery.net/tfgleCjJafHVtd2F4ngDnQ/${photo}/small 400w, https://imagedelivery.net/tfgleCjJafHVtd2F4ngDnQ/${photo}/medium 1024w, https://imagedelivery.net/tfgleCjJafHVtd2F4ngDnQ/${photo}/large 2048w`}
