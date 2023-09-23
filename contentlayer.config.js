e3e713a96371e73aeba52c94d4a08dfc8079e377
Jon Heslop <jon@jonheslop.com>
2023-09-23 23:53:25 +0100

    Light code style
    And use <code> because inline backticks is making it blow up

--- contentlayer.config.js
+++ contentlayer.config.js
@@ -77,7 +77,7 @@ export default makeSource({
       [
         rehypePrettyCode,
         {
-          theme: 'one-dark-pro',
+          theme: 'github-light',
           onVisitLine(node) {
             // Prevent lines from collapsing in `display: grid` mode, and allow empty
             // lines to be copy/pasted
