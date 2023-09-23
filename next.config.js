7bc522ed639a95f0d285dd09e27545c93bb596bb
Jon Heslop <jon@jonheslop.com>
2023-09-23 23:48:36 +0100

    Bin off a load of gross webpack warnings
    https://github.com/shadcn-ui/taxonomy/issues/100

---
+++ next.config.js
@@ -0,0 +1,10 @@
+const { createContentlayerPlugin } = require('next-contentlayer');
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {};
+
+const withContentlayer = createContentlayerPlugin({
+  // Additional Contentlayer config options
+});
+
+module.exports = withContentlayer(nextConfig);
