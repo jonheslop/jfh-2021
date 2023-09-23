42d2db615db9260f6519ff021c40144adbecac86
Jon Heslop <jon@jonheslop.com>
2023-09-23 22:09:07 +0100

    wording

--- app/colophon/page.tsx
+++ app/colophon/page.tsx
@@ -13,7 +13,7 @@ const Colophon = () => {
       <article className="md:col-start-4 space-y-4 mt-3">
         <p className="text-xl md:text-lg lg:text-xl leading-normal">
           Photo posts all shot on a <Link external href="https://en.wikipedia.org/wiki/Fujifilm_X-Pro3">Fujifilm X-Pro3</Link>.</p>
-          <p className="text-xl md:text-lg lg:text-xl leading-normal">I mostly use the Fujinon 16mm and 35mm lenses. Sometimes I use a Minolta M-Rokkor 40mm, Pentax Asahi Super-Takumar 55mm or Leica Summilux 50mm lens.
+          <p className="text-xl md:text-lg lg:text-xl leading-normal">I mostly use Fujinon XF 35mm and 16mm lenses. Sometimes I use a Minolta M-Rokkor 40mm, Pentax Asahi Super-Takumar 55mm or (Bec’s) Leica Summilux 50mm lens.
         </p>
         <p className="text-xl md:text-lg lg:text-xl leading-normal">
           Type set in <Link external href="https://klim.co.nz/retail-fonts/soehne/">Söhne</Link> by <Link external href="https://klim.co.nz/">Klim</Link>.
