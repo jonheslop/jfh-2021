d70d859c961914a5e47149f5f6f45188f285a687
Jon Heslop <jon@jonheslop.com>
2023-10-03 21:53:11 +0100

    Make arrows do something
    +/- the ID is way to simplistic. Need to make it know which photo is chronologically next/prev

--- ui/stream-overlay.tsx
+++ ui/stream-overlay.tsx
@@ -16,8 +16,8 @@ const StreamOverlay = ({photo, ...props}: Props) => {
   return (
     <>
       <Link href={`/stream`} className="fixed inset-0 bg-black/20 backdrop-blur-sm"/>
-      <div className="fixed inset-4 lg:inset-16 flex items-center justify-center pointer-events-none">
-        <button className="text-6xl text-white p-4">&larr;</button>
+      <div className="fixed inset-4 lg:inset-16 flex items-center justify-center">
+        <Link href={`/stream/${photo.id + 1}`} className="self-stretch flex items-center justify-end text-6xl text-white p-4 flex-1 hover:-translate-x-8 transition-all">&larr;</Link>
         <div className={`bg-white p-2 lg:p-6 2xl:p-8 pointer-events-auto grid ${isExpanded ? "grid-cols-3" : "grid-cols-1"} gap-2 lg:gap-4 relative w-100 group`}>
           <img
             className="w-auto max-h-[calc(100vh-12rem)] col-span-2 self-center"
@@ -29,7 +29,7 @@ const StreamOverlay = ({photo, ...props}: Props) => {
             Object.keys(parsedExif).length !== 0 && <ExifList exif={parsedExif} isExpanded={isExpanded} setExpanded={(bool:boolean) => setIsExpanded(bool)}/>
           }
         </div>
-        <button className="text-6xl text-white p-4">&rarr;</button>
+        <Link href={`/stream/${photo.id - 1}`} className="self-stretch flex items-center text-6xl text-white p-4 flex-1 hover:translate-x-8 transition-all">&rarr;</Link>
       </div>
     </>
   )
