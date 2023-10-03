e006cf8fd88605624d47933fa4e0bc0debc922c8
Jon Heslop <jon@jonheslop.com>
2023-10-03 21:41:21 +0100

    Display exif nicer

--- ui/exif-list.tsx
+++ ui/exif-list.tsx
@@ -4,20 +4,23 @@ import React, { useState } from "react";
 import { Exif } from "@/interfaces";
 import { convertToClosestFraction } from "@/lib/helpers";
 import DateFormatter from "./date";
+import Heading from "./heading";
 
 type Props = {
   exif: Exif;
+  isExpanded: boolean;
+  setExpanded: Function;
 }
 
-const ExifList = ({exif}: Props) => {
-  const [isExpanded, setIsExpanded] = useState<boolean>(false);
+const ExifList = ({exif, isExpanded, setExpanded}: Props) => {
 
   if (!isExpanded) {
-    return <button onClick={() => setIsExpanded(true)} className="bg-white/20 rounded-full w-8 h-8 absolute top-6 lg:top-8 right-6 lg:right-8 flex items-center justify-center text-white font-serif italic">i</button>
+    return <button title="Show photo exif data" onClick={() => setExpanded(true)} className="bg-white/20 rounded-full w-8 h-8 absolute top-6 lg:top-8 right-6 lg:right-8 flex items-center justify-center font-serif italic group-hover:bg-emerald-600 bg-blend-multiply">i</button>
   }
 
   return (
-    <>
+    <div className="relative">
+      <Heading level="h4" classes="mb-4">Exif</Heading>
       <ul className="list space-y-2 text-sm">
         <li><span className="text-gray-500">Camera:</span> <span>{exif.Model}</span></li>
         <li><span className="text-gray-500">Lens:</span> <span>{exif.LensModel}</span></li>
@@ -28,8 +31,8 @@ const ExifList = ({exif}: Props) => {
         <li><span className="text-gray-500">Focal length:</span> <span>{exif.FocalLengthIn35mmFormat}</span></li>
         <li><span className="text-gray-500">Time:</span> <span>{<DateFormatter includeTime dateString={exif.CreateDate} />}</span></li>
       </ul>
-      <button onClick={() => setIsExpanded(false)} className="bg-black/10 rounded-full w-8 h-8 flex items-center justify-center text-black shrink-0">×</button>
-    </>
+      <button onClick={() => setExpanded(false)} className="bg-black/10 rounded-full w-8 h-8 flex items-center justify-center text-black absolute top-0 right-0 hover:bg-emerald-600 hover:text-white">×</button>
+    </div>
   )
 }
 
--- ui/stream-overlay.tsx
+++ ui/stream-overlay.tsx
@@ -1,5 +1,7 @@
 /* eslint-disable @next/next/no-img-element */
-import React from "react";
+'use client'
+
+import React, { useState } from "react";
 import { Exif, StreamPhoto } from "@/interfaces";
 import Link from "next/link";
 import ExifList from "./exif-list";
@@ -9,21 +11,22 @@ type Props = {
 };
 
 const StreamOverlay = ({photo, ...props}: Props) => {
+  const [isExpanded, setIsExpanded] = useState<boolean>(false);
   const parsedExif: Exif = photo.exif ? JSON.parse(photo.exif?.toString()): {};
   return (
     <>
       <Link href={`/stream`} className="fixed inset-0 bg-black/20 backdrop-blur-sm"/>
       <div className="fixed inset-4 lg:inset-16 flex items-center justify-center pointer-events-none">
         <button className="text-6xl text-white p-4">&larr;</button>
-        <div className="bg-white p-2 lg:p-6 2xl:p-8 pointer-events-auto flex flex-col lg:flex-row gap-2 lg:gap-4 relative">
+        <div className={`bg-white p-2 lg:p-6 2xl:p-8 pointer-events-auto grid ${isExpanded ? "grid-cols-3" : "grid-cols-1"} gap-2 lg:gap-4 relative w-100 group`}>
           <img
-            className="w-auto max-h-[calc(100vh-12rem)]"
+            className="w-auto max-h-[calc(100vh-12rem)] col-span-2 self-center"
             alt=""
             id={`image-${photo.id}`}
             src={`https://imagedelivery.net/tfgleCjJafHVtd2F4ngDnQ/${photo.cloudflareId}/large`}/>
 
           {
-            Object.keys(parsedExif).length !== 0 && <ExifList exif={parsedExif}/>
+            Object.keys(parsedExif).length !== 0 && <ExifList exif={parsedExif} isExpanded={isExpanded} setExpanded={(bool:boolean) => setIsExpanded(bool)}/>
           }
         </div>
         <button className="text-6xl text-white p-4">&rarr;</button>
