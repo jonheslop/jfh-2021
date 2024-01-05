1426d0779dc6f4d6716fba48154fbf216a229205
Jon Heslop <jon@jonheslop.com>
2024-01-05 11:09:14 +0000

    copy change

--- app/stream/[[...photo]]/page.tsx
+++ app/stream/[[...photo]]/page.tsx
@@ -27,7 +27,7 @@ const Stream = async ({ params }: Props) => {
     <>
       <Heading classes="md:sticky top-16 text-white mix-blend-difference md:col-span-2">Stream</Heading>
       <p className="text-xl md:text-lg lg:text-xl leading-normal max-w-xl md:col-start-4 mt-3">
-        Here’s a stream of my photos, I thought would be fun to build myself somewhere to post them that wasn’t just Instagram. Mostly shot on my X-Pro3 but probably some iPhone pics too.
+        Here’s a stream of my photos, I thought would be fun to build myself somewhere to post them that wasn’t just Instagram. Mostly shot on my X-Pro3 but occasionally the odd iPhone pic too.
       </p>
       <Suspense fallback={<Loader count={8} heading={true} classes="md:col-start-2 md:col-span-3 grid-cols-2 md:grid-cols-4 lg:grid-cols-5 2xl:grid-cols-6"/>}>
         <StreamGrid photos={allPhotos} classes="md:col-span-3 md:col-start-2"/>
