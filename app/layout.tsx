7f7842088ace66da899b70acc1e50147cafcc221
Jon Heslop <jon@jonheslop.com>
2024-02-06 20:32:52 +0000

    Add new speed insights thing

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,21 +1,22 @@
-import './globals.css'
+import './globals.css';
 import { Analytics } from '@vercel/analytics/react';
-import type { Metadata } from 'next'
-import localFont from 'next/font/local'
+import type { Metadata } from 'next';
+import localFont from 'next/font/local';
 import Logo from '@/ui/logo';
 import Footer from '@/ui/footer';
+import { SpeedInsights } from '@vercel/speed-insights/next';
 
 const soehne = localFont({
   src: [
     {
       path: './fonts/soehne-web-buch.woff2',
       weight: '400',
-      style: 'normal'
-    }
+      style: 'normal',
+    },
   ],
   display: 'swap',
   variable: '--font-soehne',
-})
+});
 
 export const metadata: Metadata = {
   metadataBase: new URL('https://jonheslop.com'),
@@ -26,11 +27,11 @@ export const metadata: Metadata = {
   alternates: {
     types: {
       'application/rss+xml': 'https://jonheslop.com/feed.xml',
-    }
+    },
   },
   description: 'Jon Heslop is a front end developer based in London.',
   viewport: {
-    width: "device-width",
+    width: 'device-width',
     initialScale: 1,
   },
   openGraph: {
@@ -48,23 +49,26 @@ export const metadata: Metadata = {
     locale: 'en_GB',
     type: 'website',
   },
-}
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
-      <body className={`${soehne.variable} font-soehne bg-white dark:bg-black dark:text-white flex flex-col items-center justify-center min-h-screen`}>
+      <body
+        className={`${soehne.variable} font-soehne bg-white dark:bg-black dark:text-white flex flex-col items-center justify-center min-h-screen`}
+      >
         <div className="flex-1 px-2 py-4 md:p-8 lg:p-16 grid grid-cols-1 md:grid-cols-page-layout auto-rows-min items-start gap-16 relative w-full">
-          <Logo/>
+          <Logo />
           {children}
         </div>
-        <Footer/>
+        <Footer />
         <Analytics />
+        <SpeedInsights />
       </body>
     </html>
-  )
+  );
 }
