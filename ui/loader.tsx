c2d6b8e5c29a456439e28c37d45d1cf887c6c4b9
Jon Heslop <jon@jonheslop.com>
2024-02-18 10:02:22 +0000

    Match aspect all the time

--- ui/loader.tsx
+++ ui/loader.tsx
@@ -8,14 +8,14 @@ type Props = {
 
 const Loader = ({ count, heading, classes }: Props) => {
   return (
-    <div className={`${classes} animate-pulse grid gap-8`}>
+    <div className={`${classes} animate-pulse grid gap-2 md:gap-4 lg:gap-8`}>
       {heading ? (
         <span className="col-span-8">
           <span className="block bg-gray-200 w-44 h-8 rounded-xl"></span>
         </span>
       ) : null}
       {[...Array(count)].map(() => (
-        <div key={crypto.randomUUID()} className="bg-gray-200 h-[185px]" />
+        <div key={crypto.randomUUID()} className="bg-gray-200 aspect-[2/3]" />
       ))}
     </div>
   );
